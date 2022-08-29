e96bb7ee15d67ff7021079024559cf308693611a
Paul Willis <ios_dev@paulrobertwillis.com>
2022-08-29 20:57:33 +0100

    [APP-0006] Refactors and restructures files and folders related to SearchList; adds initial SearchListViewModel to complete MVVM design of SearchList

---
+++ RESTfulRockTracks/Presentation/SearchScene/SearchList/View/SearchListViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SearchListViewController.swift
+//  RESTfulRockTracks
+//
+//  Created by Paul Willis on 25/08/2022.
+//
+
+import UIKit
+
+class SearchListViewController: UIViewController {
+    
+    // MARK: - Private Properties
+    
+    private let viewModel: SearchViewModel = SearchViewModel()
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ RESTfulRockTracks/Presentation/SearchScene/SearchList/ViewModel/SearchListViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  SearchListViewModel.swift
+//  RESTfulRockTracks
+//
+//  Created by Paul Willis on 29/08/2022.
+//
+
+import Foundation
+
+class SearchListViewModel {
+    
+}
