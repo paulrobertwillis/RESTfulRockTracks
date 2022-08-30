7473137fc7923057133b3caadcc0f15adb2b14bc
Paul Willis <paul.willis@mkodo.com>
2022-08-30 11:23:16 +0100

    [APP-0011] Adds MVVM classes for SearchDetails: SearchDetailsViewController storyboard and class, and SearchDetailsViewModel

---
+++ RESTfulRockTracks/Presentation/SearchScene/SearchDetails/View/SearchDetailsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SearchDetailsViewController.swift
+//  RESTfulRockTracks
+//
+//  Created by Paul on 30/08/2022.
+//
+
+import UIKit
+
+class SearchDetailsViewController: UIViewController, StoryboardInstantiable {
+
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    
+}
---
+++ RESTfulRockTracks/Presentation/SearchScene/SearchDetails/ViewModel/SearchDetailsViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  SearchDetailsViewModel.swift
+//  RESTfulRockTracks
+//
+//  Created by Paul on 30/08/2022.
+//
+
+import Foundation
+
+class SearchDetailsViewModel {
+    
+}
