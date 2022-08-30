beeeb69096db9481faf6217c253d08f7397c845c
Paul Willis <paul.willis@mkodo.com>
2022-08-30 09:37:49 +0100

    [APP-0008] Adds RawDataResponseDecoder for decoding raw Data such as images

--- RESTfulRockTracks/Data/Network/DataTransferService/ResponseDecoders.swift
+++ RESTfulRockTracks/Data/Network/DataTransferService/ResponseDecoders.swift
@@ -1,5 +1,5 @@
 //
-//  ResponseDecoder.swift
+//  ResponseDecoders.swift
 //  RESTfulRockTracks
 //
 //  Created by Paul Willis on 29/08/2022.
@@ -11,13 +11,28 @@ public protocol ResponseDecoder {
     func decode<T: Decodable>(_ data: Data) throws -> T
 }
 
-class JSONResponseDecoder {
-    // MARK: - Private Properties
+// MARK: - JSONResponseDecoder
+class JSONResponseDecoder: ResponseDecoder {
     private let jsonDecoder = JSONDecoder()
-}
-
-extension JSONResponseDecoder: ResponseDecoder {
+    
     public func decode<T: Decodable>(_ data: Data) throws -> T {
         return try jsonDecoder.decode(T.self, from: data)
+    }
+}
+
+// MARK: - RawDataResponseDecoder
+class RawDataResponseDecoder: ResponseDecoder {
+    
+    enum CodingKeys: String, CodingKey {
+        case `default` = ""
+    }
+
+    public func decode<T: Decodable>(_ data: Data) throws -> T {
+        if T.self is Data.Type, let data = data as? T {
+            return data
+        } else {
+            let context = DecodingError.Context(codingPath: [CodingKeys.default], debugDescription: "Expected Data type")
+            throw Swift.DecodingError.typeMismatch(T.self, context)
+        }
     }
 }
